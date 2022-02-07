3e0c59a88ff548e7ac06546e07be73cfe98ab918
Anthony Sonrel <anthony.sonrel@uzh.ch>
2022-02-07 15:27:38 +0100

    fix

--- omnibench-method/install.R
+++ omnibench-method/install.R
@@ -1,10 +1,25 @@
+
+#################
+# YOUR PACKAGES #
+#################
+
+
+
+
+
+
+
+#####################
+## DO NOT MODIFIY ###
+#####################
+# These are required by the example datasets.
+
 install.packages("BiocManager")
 install.packages("https://cran.rstudio.com/src/contrib/matrixStats_0.58.0.tar.gz", repo=NULL, type="source")
 BiocManager::install('batchelor')
 BiocManager::install('SingleCellExperiment')
 BiocManager::install('jsonlite')
 BiocManager::install('Matrix')
-
 install.packages("dplyr")
 install.packages("magrittr")
 install.packages("R.utils")
