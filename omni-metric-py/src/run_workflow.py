6cb5f099003d60e4a63217e7ec0c4e4e3e6fd4db
Anthony Sonrel <anthony.sonrel@uzh.ch>
2022-09-04 14:39:20 +0200

    fix

--- omni-metric-py/src/run_workflow.py
+++ omni-metric-py/src/run_workflow.py
@@ -34,7 +34,7 @@ renku_save()
 ###################### Generate info file ######################
 
 omni_info = get_omni_object_from_yaml('src/info_config.yaml')
-omni_info.wflow_name = "{{__sanitized_project_name}}_info"
+omni_info.wflow_name = "{{__sanitized_project_name__}}_info"
 
 ## Check object
 print(
