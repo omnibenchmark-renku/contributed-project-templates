05e97ca0658c4ddc6fedf11fe543f1982becfe0d
Anthony Sonrel <anthony.sonrel@uzh.ch>
2022-10-12 18:21:33 +0200

    simplification of run comb

--- omni-metric-py/src/run_workflow.py
+++ omni-metric-py/src/run_workflow.py
@@ -1,5 +1,6 @@
 from omnibenchmark.utils.build_omni_object import get_omni_object_from_yaml
 from omnibenchmark.renku_commands.general import renku_save
+from omnibenchmark.management.data_commands import update_dataset_files
 renku_save()
 
 ## Load config
@@ -28,31 +29,8 @@ omni_obj.run_renku()
 renku_save()
 
 ## Update Output dataset
+meta_files = [files["meta_file"] for files in omni_obj.inputs.input_files.values()]
+update_dataset_files(urls=meta_files, dataset_name=omni_obj.name)
 omni_obj.update_result_dataset()
 renku_save()
 
-###################### Generate info file ######################
-
-omni_info = get_omni_object_from_yaml('src/info_config.yaml')
-omni_info.wflow_name = "{{__sanitized_project_name__}}_info"
-
-## Check object
-print(
-    f"Object attributes: \n {omni_info.__dict__}\n"
-)
-print(
-    f"File mapping: \n {omni_info.outputs.file_mapping}\n"
-)
-print(
-    f"Command line: \n {omni_info.command.command_line}\n"
-)
-
-## Run workflow
-omni_info.run_renku()
-renku_save()
-
-## Update Output dataset
-omni_info.update_result_dataset()
-renku_save()
-
-################################################################
