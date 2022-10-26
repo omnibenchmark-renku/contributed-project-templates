15fdf6e64613dbc74c4f09d3fc4b2ac6620688d0
Anthony Sonrel <anthony.sonrel@uzh.ch>
2022-10-26 07:45:26 +0200

    remove comms

--- metric_summary/src/generate_json_from_res_files.py
+++ metric_summary/src/generate_json_from_res_files.py
@@ -24,17 +24,6 @@ print("endpoint:", ENDPOINT)
 print("no ds:", NO_DS_IN_PARAMS)
 print("out:", OUT_JSON)
 print("int:", INPUT[0:4], "...[truncated]")
-## ONLY FOR TESTING ------------------------------------------------------------------------
-# INPUT = ['data/cms_py/cms_py_inst0_omni_mnn_py_inst0_cellbench_data_py___order_size__hvg_all__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst1_omni_mnn_py_inst0_cellbench_data_py___order_size__hvg_all__d_30__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst2_omni_mnn_py_inst0_cellbench_data_py___order_size__hvg_sig__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst3_omni_mnn_py_inst0_cellbench_data_py___order_size__hvg_sig__d_30__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst4_omni_mnn_py_inst0_cellbench_data_py___order_size__hvg_none__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst5_omni_mnn_py_inst0_cellbench_data_py___order_size__hvg_none__d_30__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst6_omni_mnn_py_inst0_cellbench_data_py___order_inverse__hvg_all__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst7_omni_mnn_py_inst0_cellbench_data_py___order_inverse__hvg_all__d_30__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst8_omni_mnn_py_inst0_cellbench_data_py___order_inverse__hvg_sig__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst9_omni_mnn_py_inst0_cellbench_data_py___order_inverse__hvg_sig__d_30__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst10_omni_mnn_py_inst0_cellbench_data_py___order_inverse__hvg_none__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst11_omni_mnn_py_inst0_cellbench_data_py___order_inverse__hvg_none__d_30__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst12_omni_mnn_py_inst0_cellbench_data_py___order_auto__hvg_all__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst13_omni_mnn_py_inst0_cellbench_data_py___order_auto__hvg_all__d_30__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst14_omni_mnn_py_inst0_cellbench_data_py___order_auto__hvg_sig__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst15_omni_mnn_py_inst0_cellbench_data_py___order_auto__hvg_sig__d_30__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst16_omni_mnn_py_inst0_cellbench_data_py___order_auto__hvg_none__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst17_omni_mnn_py_inst0_cellbench_data_py___order_auto__hvg_none__d_30__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst18_omni_mnn_py_inst1_csf_patient_py___order_size__hvg_all__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst19_omni_mnn_py_inst1_csf_patient_py___order_size__hvg_all__d_30__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst20_omni_mnn_py_inst1_csf_patient_py___order_size__hvg_sig__d_10__k_20_corrected_dim_file_metric_res.json', 'data/cms_py/cms_py_inst21_omni_mnn_py_inst1_csf_patient_py___order_size__hvg_sig__d_30__k_20_corrected_dim_file_metric_res.json']
-
-# Triplestore endpoint for queries
-# ENDPOINT = "http://imlspenticton.uzh.ch/omni_batch_py_sparql"
-# remove dataset from parameter ? (assumes that name is 'dataset_name')
-# NO_DS_IN_PARAMS = True
-# name of output JSON
-# OUT_JSON = "test.json"
-
-## ---------------------------------------------------------------------------------
 
 
 json_out = dict()
@@ -43,13 +32,7 @@ dir_names = []
 for NAM in INPUT:
     
     print("Getting summary info for:", NAM)
-    # TODO: recognize if not in graph and next + warning (do with cms here)
-    # TODO: add option to add other info such as date created, time run,e tc...?
-    # TODO: atm, removes automatically 'dataset_name' from parameters
-    # TODO: see how it handles if some results were generated with different parameters
-    # TODO: see if 'processed/meta_' still a problem in new triplestore
-    # TODO: parallelization for speed
-    
+   
     # DIRECTORY: save for latter to retrieve _info
     dir_i = os.path.dirname(NAM)
     if dir_i not in dir_names: 
@@ -148,8 +131,6 @@ json_out = json.dumps(json_out, indent = 3)
 with open(OUT_JSON+".json", "w") as outfile:
     outfile.write(json_out)
     
-  
-
 
 # check if we have 1 info file per directory
 for i in dir_names:
