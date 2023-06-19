396c0ac730128d87669901932388b04fd3610862
Anthony Sonrel <thony.s@hotmail.com>
2023-06-19 13:52:25 +0200

    simplify R installation

--- omni-data-py/install.R
+++ omni-data-py/install.R
@@ -1,30 +1,16 @@
 
-install.packages("BiocManager")
-
-BiocManager::install(version = "3.8")
-
+#install optparse 
 install.packages('https://cran.r-project.org/src/contrib/Archive/getopt/getopt_1.20.2.tar.gz')
 install.packages("https://cran.r-project.org/src/contrib/Archive/optparse/optparse_1.6.0.tar.gz")
 
-install.packages("remotes")
-remotes::install_version("styler", version = "1.0.1")
-remotes::install_version("usethis", version = "1.3.0")
-remotes::install_version("devtools", version = "1.13.5")
 
+# install Bioc
+install.packages("BiocManager")
+BiocManager::install(version = "3.8")
+
+# install project dependencies
 BiocManager::install('SingleCellExperiment')
+BiocManager::install('DuoClustering2018')
 BiocManager::install('jsonlite')
 BiocManager::install('Matrix')
-
-install.packages("dplyr")
-install.packages("magrittr")
-
-install.packages("https://cran.r-project.org/src/contrib/Archive/locfit/locfit_1.5-9.1.tar.gz", repos=NULL, dependencies = TRUE)
-
-remotes::install_version("RcppAnnoy", version = "0.0.16")
-
-
-
-
-
-install.packages("R.utils")
-
+BiocManager::install("R.utils")
