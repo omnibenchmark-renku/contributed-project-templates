e635a0c66811a59299f3bb8dc28a52e33134f1fc
Anthony Sonrel <thony.s@hotmail.com>
2023-06-26 12:30:49 +0200

    more tidy

--- omni-method-py/install.R
+++ omni-method-py/install.R
@@ -1,11 +1,9 @@
 install.packages("BiocManager")
 install.packages("optparse")
 install.packages("https://cran.rstudio.com/src/contrib/matrixStats_0.58.0.tar.gz", repo=NULL, type="source")
-BiocManager::install('batchelor')
-BiocManager::install('SingleCellExperiment')
-BiocManager::install('jsonlite')
-BiocManager::install('Matrix')
 
-install.packages("dplyr")
-install.packages("magrittr")
-install.packages("R.utils")
+pkgs <- c("batchelor","SingleCellExperiment",
+          "jsonlite","Matrix",
+          "dplyr","magrittr","R.utils")
+
+BiocManager::install(pkgs)
